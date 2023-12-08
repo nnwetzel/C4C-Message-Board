8c385d4a79fc0eb1f2c19ef3508b406bb05b13f5
Nat Wetzel <50302468+nnwetzel@users.noreply.github.com>
2023-12-08 00:17:43 -0500

    Test

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-var threads = [
+var defaultThreads = [
     {
         id: 1,
         title: "C4C Board",
@@ -13,13 +13,29 @@ var threads = [
             },
         ]
     },
-]
+];
 
 var threads;
-if (localStorage && localStorage.getItem ('threads')) {
-   threads = JSON.parse(localStorage.getItem('threads'));
+
+// Check if there are threads in localStorage
+if (localStorage && localStorage.getItem('threads')) {
+    threads = JSON.parse(localStorage.getItem('threads'));
 } else {
+    // If not, use defaultThreads and store it in localStorage
     threads = defaultThreads;
-    localStorage.setItem ('threads', JSON.stringify(defaultThreads));
+    localStorage.setItem('threads', JSON.stringify(defaultThreads));
 }
-//localStorage.clear();
+
+// Add a new comment to the first thread in defaultThreads
+var newComment = {
+    author: "John",
+    date: new Date(),
+    content: "This is a new comment.",
+};
+
+defaultThreads[0].comments.push(newComment);
+
+// Save the updated defaultThreads to localStorage
+localStorage.setItem('threads', JSON.stringify(defaultThreads));
+
+// Now, both threads and defaultThreads have the same data, and changes are reflected in localStorage.
