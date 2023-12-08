2962b21d3e664463055ccda1f333d619471e5ef2
Nat Wetzel <50302468+nnwetzel@users.noreply.github.com>
2023-12-08 00:11:10 -0500

    localStorage

--- data.js
+++ data.js
@@ -15,46 +15,11 @@ var threads = [
     },
 ]
 
-document.addEventListener('DOMContentLoaded', async function () {
-    var threads;
-
-    // Try to get threads from localStorage
-    const storedThreads = localStorage.getItem('threads');
-
-    if (storedThreads) {
-        // If threads are stored in localStorage, use them
-        threads = JSON.parse(storedThreads);
-        updateUI();
-    } else {
-        try {
-            // Fetch threads from the server
-            const response = await fetch('https://nnwetzel.github.io/C4C-Message-Board/thread');
-            const data = await response.json();
-
-            // Assuming the data received is an array of threads
-            threads = data;
-
-            // Cache threads in localStorage
-            localStorage.setItem('threads', JSON.stringify(threads));
-
-            // Call a function to initialize or update the UI with the fetched threads
-            updateUI();
-        } catch (error) {
-            console.error('Error fetching threads:', error);
-
-            // If there's an error fetching threads, use defaultThreads or handle it accordingly
-            threads = defaultThreads;
-
-            // Call a function to initialize or update the UI with the default threads
-            updateUI();
-        }
-    }
-
-    function updateUI() {
-        // Add code here to update the UI with the threads data
-        // For example, you can render the threads in the HTML or perform other UI-related tasks
-    }
-});
-
-
+var threads;
+if (localStorage && localStorage.getItem ('threads')) {
+   threads = JSON.parse(localStorage.getItem('threads'));
+} else {
+    threads = defaultThreads;
+    localStorage.setItem ('threads', JSON.stringify(defaultThreads));
+}
 //localStorage.clear();
