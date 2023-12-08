1e8e9966a60633fa32c8bdc57533e5d7a31d17b7
Nat Wetzel <50302468+nnwetzel@users.noreply.github.com>
2023-12-07 22:32:40 -0500

    Initial commit

---
+++ data.js
@@ -0,0 +1,25 @@
+var threads = [
+    {
+        id: 1,
+        title: "C4C Board",
+        author: "Nat",
+        date: new Date("2023-12-07T21:05:21"),
+        content: "Board content",
+        comments: [
+            {
+                author: "Nat",
+                date: new Date("2023-12-07T21:05:21"),
+                content: "Hi, my name is Nathaniel Wetzel. Welcome to my MVP messaging board application for C4C.",
+            },
+        ]
+    },
+]
+
+var threads;
+if (localStorage && localStorage.getItem ('threads')) {
+   threads = JSON.parse(localStorage.getItem('threads'));
+} else {
+    threads = defaultThreads;
+    localStorage.setItem ('threads', JSON.stringify(defaultThreads));
+}
+//localStorage.clear();
